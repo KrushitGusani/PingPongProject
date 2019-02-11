46011acb3fdc6a390f9c77e8ae0fef67aed8944a
Krushit <Krushit@DESKTOP-CICCLTS>
2019-02-11 05:29:05 -0500

    Player Model-Commit

---
+++ PingPongProject/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PingPongProject.Models
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerID { get; set; }
+        [Required, Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required, Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [Display(Name = "Skill Level")]
+        public string SkillLevel { get; set; }
+        [Required, Display(Name = "Email Address"), EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string EmailAddress { get; set; }
+    }
+}
